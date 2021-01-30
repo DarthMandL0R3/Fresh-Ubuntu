8ca92668faea4a2c30dfd6b07490790693659e1e
abrar <abrar@localhost>
2021-01-31 06:59:36 +0800

    revamping the script after testing

--- fresh-ubuntu20.04-all.sh
+++ fresh-ubuntu20.04-all.sh
@@ -1,71 +1,107 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-###WARNING: Please run this script as root user!!!###
+###Warning: Please run this script as root user###
 
-#Author: Abrar and Geng
-#Version: 1.0
+#Author: Abrar and Geng.
+#Version: 1.1.
 
-#Description: Post-Install script for Ubunu Systems.
+#Description: Post-install script for Ubuntu systems.
 
-if cat /root/.fresh-install
+file="/root/.fresh-install"
+
+if [ ! -f "$file" ]
 then
-        exit
+	echo "$0: File '${file}' has not been created. Proceeding with post-install."
 else
-        clear
+	echo "$0: File '${file}' has been created. DO NOT PROCEED WITH THIS SCRIPT."
 fi
 
+###Update and Upgrade All Packages###
+echo "Update all packages.."
+echo "---------------------"
+apt update && apt upgrade -y
+sleep 5s
+echo "---------------------"
+echo "All packages updated"
+
+###Install Essential Packages###
+echo "Installing essential packages.."
+echo "-------------------------------"
+sleep 5s
+apt install -y build-essential git
+sleep 5s
+apt install -y screen wget mlocate nmap mtr vim lynx chrony net-tools bind9-utils telnet sysstat firewalld
+sleep 5s
+apt install -y libpython2-stdlib libpython2.7-minimal libpython2.7-stdlib libuser1 python-apt python-dbus python-gi python-is-python2 python2 python2-minimal python2.7 python2.7-minimal qemu-guest-agent usermode
+sleep 5s
+apt install -y oddjob-mkhomedir
+sleep 5s
+apt install -y freeipa-client
+echo "----------------------------"
+echo "Essential packages installed"
+
 ###Enable root Login###
-echo -e "Enabling root login.."
-sleep 1
+echo "Enabling root login.."
+echo "---------------------"
 cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart sshd
-
-###Install Essential Packages###
-echo -e "Installing essential packages.."
-sleep 1
-apt install -y build-essential git
-sleep 2
-apt install -y screen wget mlocate nmap mtr vim lynx chrony net-tools bind9-utils telnet sysstat firewalld
-sleep 2
-apt install -y python-dev python-pip python-apt python-dbus usermode qemu-guest-agent
-sleep 2
-apt install -y freeipa-client oddjob-mkhomedir
+sleep 5s
+echo "------------------"
+echo "Root login enabled"
 
 ###Disable Firewall###
-echo -e "Disabling Firewall and allowing Nimsoft.."
-sleep 1
+echo "Disabling Firewall and allowing Nimsoft.."
+echo "-----------------------------------------"
 ufw status verbose
 systemctl stop ufw
 systemctl disable ufw
+sleep 5s
 /usr/bin/firewall-cmd --zone=public --add-rich-rule='rule family=ipv4 source address=172.27.0.79 accept' --permanent
 /usr/bin/firewall-cmd --reload
 systemctl stop firewalld
 systemctl disable firewalld
+sleep 5s
+echo "-----------------"
+echo "Firewall Disabled"
 
 ###Edit ULimit###
-echo -e "Editing ULimit.."
-sleep 1
-echo -e "* hard\tnofile\t64000" >> /etc/security/limits.conf
-echo -e "* soft\tnofile\t64000" >> /etc/security/limits.conf
+echo "Editing ULimit.."
+echo "----------------"
+echo "* hard\tnofile\t64000" >> /etc/security/limits.conf
+echo "* soft\tnofile\t64000" >> /etc/security/limits.conf
+sleep 5s
+echo "-------------"
+echo "ULimit Edited"
 
 ###Disable IPv6###
-echo -e "Disable IPv6.."
-sleep 1
-cp -a /etc/sysctl.conf /etc/sysctl.conf.bak
+echo "Disable IPv6.."
+echo "--------------"
+cp -p /etc/sysctl.conf /etc/sysctl.conf.bak
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 sysctl -p
+sleep 5s
+echo "-------------"
+echo "IPv6 Disabled"
 
 ###Install oVirt Guest Agent###
-echo -e "Installing oVirt Guest Agent.."
-sleep 1
+echo "Installing oVirt Guest Agent.."
+echo "------------------------------"
 wget http://archive.ubuntu.com/ubuntu/pool/universe/p/python-ethtool/python-ethtool_0.12-1.1_amd64.deb
 dpkg -i python-ethtool_0.12-1.1_amd64.deb
-sleep 2
+sleep 5s
 wget http://security.ubuntu.com/ubuntu/pool/universe/o/ovirt-guest-agent/ovirt-guest-agent_1.0.13.dfsg-2_all.deb
 dpkg -i ovirt-guest-agent_1.0.13.dfsg-2_all.deb
+sleep 5s
+echo "---------------------------"
+echo "Ovirt Guest Agent Installed"
 
 echo "THIS SCRIPT ALREADY RUN BY $(whoami) at $(date)" > /root/.fresh-install
+sleep 5s
+
+echo "############################################"
+echo "The script have been completed successfully."
+echo "############################################"
 
 exit 0
